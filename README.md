51b9de90d469687b9ece6885f92f063135c46aa2
gebbing12 <127727594+gebbing12@users.noreply.github.com>
2024-07-01 12:03:15 -0700

    Airflow (#1)
    * Change gitignore
    
    * Edit read README file
    
    * Aiflow dag code
    
    * Update README.md
    
    * delect ignore
    
    * Edit Airflow code and requirement file
    
    * Add MySQL transaction operator and edit README
    
    * Add unit test
    
    * Delect oncokb-variant-recommendation_test.py

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # oncokb-variant-recommendation
 
-## Running Locally
+## Set up python environment
+### Running Locally
 
 1. First Time Setup
 
@@ -37,7 +38,7 @@
    pip install -r requirements.txt
    ```
 
-## Running Tests
+### Running Tests
 
 ```sh
 pytest
@@ -45,8 +46,76 @@ pytest
 
 - Note that all test files must end in `_test.py`
 
-## Create/Update requirements file
+
+### Create/Update requirements file
 
 ```sh
 pip freeze > requirements.txt
 ```
+
+## Set Airflow
+### Install Airflow 
+```sh
+pip install 'apache-airflow==2.9.1' \
+ --constraint "https://raw.githubusercontent.com/apache/airflow/constraints-2.9.1/constraints-3.8.txt"
+```
+- Change constraints-3.8.txt to your python version
+- More details in [Airflow document](https://airflow.apache.org/docs/apache-airflow/stable/installation/index.html#using-pypi)
+
+### Initialize Airflow
+1. Create an airflow folder under your object, and create a dags folder under airflow. 
+
+2. To configure Airflow to recognize your DAGs directory, you need to set the `AIRFLOW_HOME` environment variable. replacing `/path/to/dags/folder/parent/folder` with the actual path to your desired directory:
+   
+```sh
+export AIRFLOW_HOME= path/to/dags/flod/parent/flod
+```
+
+3. Run Airflow Standalone, and get the default username and password.
+
+```sh
+airflow standalone
+```
+
+The command initializes the database, creates a user, and starts all components.
+
+- If you prefer to run individual components of Airflow manually, or if you need personalized user information, instead of using the all-in-one standalone command, you can run the following:
+```sh
+airflow db init
+
+airflow users create \
+    --username admin \
+    --firstname Peter \
+    --lastname Parker \
+    --role Admin \
+    --email spiderman@superhero.org
+
+airflow webserver --port 8080
+
+airflow scheduler
+
+airflow triggerer
+```
+- More details in [Airflow document](https://airflow.apache.org/docs/apache-airflow/stable/start.html)
+
+4. Access the Airflow UI:
+Visit localhost:8080 in your browser.
+
+5. Connect to AWS S3: Choose connections under Admin, create a new connection. Input `oncokb_s3` in `Connection Id`, choose `Connection Type` as Amazon Web Services , and input `AWS Access Key ID` and `AWS Secret Access Key`.
+
+6. Connect to MySQL: Choose connections under Admin, create a new connection. Input `oncokb_mysql` in `Connection Id`, choose `Connection Type` as MySQL , and input`Host`,`schema`,`login`,`password` and `port`.
+
+### Note:
+- If you want to close all Airflow DAG or connectionsexamples on Airflow webserver. Open airflow.cfg and change `load_examples = False` or `load_default_connections = False`.
+
+## Test Airflow
+With the Airflow CLI, run to test your dag, you can check the result and logs at Airflow UI.
+```sh
+airflow dags test <dag_id>
+```
+
+You can use CLI to list all dags you have.
+```
+airflow dags list
+```
+
---
+++ airflow/dags/oncokb_curation.py
@@ -0,0 +1,198 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.providers.mysql.operators.mysql import MySqlOperator
+from airflow.providers.mysql.hooks.mysql import MySqlHook
+from airflow.io.path import ObjectStoragePath
+from collections import defaultdict
+import pandas as pd
+import logging
+import os 
+
+# Aieflow Object Storage path to s3
+base = ObjectStoragePath("s3://oncokb_s3@oncokb-ge")
+# Path to the file containing samples to be filtered; leave as '' to skip filtering
+filter_path = ''
+
+def upload_to_s3(filename,df):
+    """
+    Function to upload DataFrame to AWS S3 bucket.
+    Args:
+        path: file name in s3 bucket
+        df: DataFrame to upload
+    """
+    path = base / filename
+    with path.open("w") as file:
+        df.to_csv(file, sep='\t', index=False)
+
+def read_from_s3(filename):
+    """
+    Function to download file from AWS S3 bucket.
+    Args:
+        file_name: file name in s3 bucket
+    Return: 
+        DataFrame for file
+    """
+    path = base / filename
+    with path.open() as f:
+        df = pd.read_csv(f,sep='\t')
+    return df
+
+def exclude_samples(data,filter_path):
+    """
+    Function to filter the data.
+    Args:
+        data: DataFrame need to be filter
+        filter_path: filter_path
+    Return: 
+        DataFrame after filter
+    """
+    excluded_sample_ids = pd.read_csv(filter_path,delimiter='\t',header=None)[0].tolist()
+    filtered_data = data[~data['SAMPLE_ID'].isin(excluded_sample_ids)]
+    return filtered_data
+
+def get_data(testname,filter=False):
+    '''
+    Get Sample, Mutation and Filter data from MSK API in the future
+    Args:
+        testname: test's name
+        filter: Whether to filter data, default is False
+    Return: 
+        DataFrame with column of ['SAMPLE_ID', 'CANCER_TYPE','CANCER_TYPE_DETAILED','Hugo_Symbol','Entrez_Gene_Id','HGVSp_Short']
+    '''
+
+def get_statistic_file(ti):
+        test_name="msk_impact_2017"
+        data=get_data(test_name)
+        
+        # All cancer frequency
+        all_number = data[['Hugo_Symbol','Entrez_Gene_Id', 'HGVSp_Short']].value_counts().reset_index(name='Number')
+        all_percentage = data[['Hugo_Symbol','Entrez_Gene_Id','HGVSp_Short']].value_counts(normalize=True).reset_index(name='Percentage')
+        all_frequency = pd.merge(all_number, all_percentage, on=['Hugo_Symbol','Entrez_Gene_Id','HGVSp_Short'])
+        upload_to_s3(f"{test_name}_all_frequency.txt",all_frequency)
+        logging.info("upload all_frequecny file")
+
+        # Cancer type frequency
+        type_number = data.groupby('CANCER_TYPE')[['Hugo_Symbol', 'Entrez_Gene_Id','HGVSp_Short']].value_counts().reset_index(name='Number')
+        type_percentage = data.groupby('CANCER_TYPE')[['Hugo_Symbol', 'Entrez_Gene_Id','HGVSp_Short']].value_counts(normalize=True).reset_index(name='Percentage')
+        type_frequency = pd.merge(type_number, type_percentage, on=['CANCER_TYPE', 'Hugo_Symbol', 'Entrez_Gene_Id','HGVSp_Short'])
+        upload_to_s3(f"{test_name}_type_frequency.txt",type_frequency)
+        logging.info("upload type_frequecny file")
+
+        # Cancer type detailed frequency
+        detailed_number = data.groupby(['CANCER_TYPE','CANCER_TYPE_DETAILED'])[['Hugo_Symbol', 'Entrez_Gene_Id','HGVSp_Short']].value_counts().reset_index(name='Number')
+        detailed_percentage = data.groupby(['CANCER_TYPE','CANCER_TYPE_DETAILED'])[['Hugo_Symbol', 'Entrez_Gene_Id','HGVSp_Short']].value_counts(normalize=True).reset_index(name='Percentage')
+        detailed_frequency = pd.merge(detailed_number, detailed_percentage, on=['CANCER_TYPE', 'CANCER_TYPE_DETAILED', 'Hugo_Symbol', 'Entrez_Gene_Id','HGVSp_Short'])
+        upload_to_s3(f"{test_name}_detailed_frequency.txt",detailed_frequency)
+        logging.info("upload detrailed_frequecny file")
+
+        ti.xcom_push(key="test_name", value = test_name)
+
+def merge_mutation(ti):
+        # Get statistic data
+        test_name = ti.xcom_pull(task_ids="get_statistic_file", key="test_name")
+        all_frequency = read_from_s3(f"{test_name}_all_frequency.txt")
+        type_frequency = read_from_s3(f"{test_name}_type_frequency.txt")
+        detailed_frequency = read_from_s3(f"{test_name}_detailed_frequency.txt")
+
+        # Set mutation as index
+        type_frequency.set_index(['Hugo_Symbol', 'HGVSp_Short'], inplace=True)
+        detailed_frequency.set_index(['Hugo_Symbol', 'HGVSp_Short'], inplace=True)
+
+        new_rows = []
+        for row in all_frequency.itertuples(index=False):
+            new_row = {
+                "entrez_gene_id":row.Entrez_Gene_Id,
+                "hugo_symbol": row.Hugo_Symbol,
+                "variant": row.HGVSp_Short,
+                "criteria_all": {
+                    'allcancertypefrequency': {
+                        'number': row.Number,
+                        'percentage': row.Percentage
+                    }
+                }
+            }
+
+            # get cancer type
+            type_df = type_frequency.loc[(row.Hugo_Symbol, row.HGVSp_Short)]
+            type_json = defaultdict(dict)
+            for type_row in type_df.itertuples(index=False):
+                type_json[type_row.CANCER_TYPE]['number'] = type_row.Number
+                type_json[type_row.CANCER_TYPE]['percentage'] = type_row.Percentage
+            new_row["criteria_cancer_type"] = dict(type_json)
+
+            # get cancer detailed
+            detailed_df = detailed_frequency.loc[(row.Hugo_Symbol, row.HGVSp_Short)]
+            detailed_json = defaultdict(dict)
+            for detailed_row in detailed_df.itertuples(index=False):
+                detailed_json[detailed_row.CANCER_TYPE_DETAILED]['number'] = detailed_row.Number
+                detailed_json[detailed_row.CANCER_TYPE_DETAILED]['percentage'] = detailed_row.Percentage
+            new_row["criteria_cancer_type_detailed"] = dict(detailed_json)
+
+            new_rows.append(new_row)
+
+        merge_table = pd.DataFrame(new_rows)
+        upload_to_s3(f"{test_name}_merge_frequency.txt",merge_table)
+        logging.info("upload merge_frequecny file")
+
+def load_csv_to_mysql(ti):
+    test_name = ti.xcom_pull(task_ids="get_statistic_file", key="test_name")
+    merge_frequency = read_from_s3(f"{test_name}_merge_frequency.txt")
+
+    # Change DataFrame nan to None
+    data_to_insert = [
+        tuple(None if pd.isna(x) else x for x in row)
+        for row in merge_frequency.itertuples(index=False)
+        ]
+    
+    insert_query = """
+        INSERT INTO variant_recommendation (entrez_gene_id, hugo_symbol, variant, criteria_all, criteria_cancer_type, criteria_cancer_type_detailed)
+        VALUES (%s, %s, %s, %s, %s, %s) ;
+        """
+
+    # Connect to MySQL
+    mysql_hook = MySqlHook(mysql_conn_id='oncokb_mysql')
+    conn = mysql_hook.get_conn()
+    cursor = conn.cursor()
+    try:
+        cursor.execute("START TRANSACTION;")
+        cursor.execute("DELETE FROM variant_recommendation;")
+        cursor.executemany(insert_query, data_to_insert)
+        conn.commit()
+        logging.info("save merge_frequecny file to MySQL")
+    except Exception as e:
+        conn.rollback()
+        logging.info(f"An error occurred: {e}")
+    finally:
+        cursor.close()
+        conn.close()
+    
+with DAG(
+    dag_id="oncokb_curation_v1",
+    description="Get the statistic file and save to S3",
+    start_date=datetime.now() - timedelta(days=1),
+    schedule_interval="@weekly",
+    catchup=False,
+    default_args = {
+    	'owner': 'oncoKB',
+		'retries': 3,
+		'retry_delay': timedelta(minutes=5)
+    }
+) as dag:
+    
+    get_statistic_file_task = PythonOperator(
+          task_id = 'get_statistic_file',
+           python_callable = get_statistic_file
+    )
+
+    aggregrate_statistic_file_task = PythonOperator(
+          task_id = 'aggregrate_statistic_file',
+           python_callable = merge_mutation
+    )
+
+    upload_mysql_task = PythonOperator(
+        task_id='load_csv_to_mysql',
+        python_callable=load_csv_to_mysql
+    )
+    
+    get_statistic_file_task >> aggregrate_statistic_file_task >> upload_mysql_task
